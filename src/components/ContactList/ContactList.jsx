d10ce9ea387a3d74ecd59e56a3ece817bc8d1692
RuslanMiki <mikitchenko_ruslan@ukr.net>
2023-01-24 18:26:26 +0100

    finish

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+import ContactItem from '../ContactItem/ContactItem';
+import styles from './ContactList.module.scss';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={styles.list}>
+      {contacts.map(contact => (
+        <ContactItem
+          contact={contact}
+          onDeleteContact={onDeleteContact}
+          key={contact.id}
+        />
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
